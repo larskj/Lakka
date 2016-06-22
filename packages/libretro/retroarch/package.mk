287545ac151b57f0da7c4337da8b43c24fcbad10
Lars Kjærgaard <larskj@gmail.com>
2016-06-22 15:28:42 +0200

    Update RetroArch to 8a8fadc
    - To get the new swapchain_images setting which can
      be used to disable triple buffering.

--- packages/libretro/retroarch/package.mk
+++ packages/libretro/retroarch/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="retroarch"
-PKG_VERSION="af4d085"
+PKG_VERSION="8a8fadc"
 PKG_REV="3"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv3"
